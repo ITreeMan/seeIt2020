141b325c85d3562683743c7b637f3c60163a5707
ITreeMan <p_anusorn@kkumail.com>
2020-01-07 22:21:41 +0700

    al

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+import { FormNewsComponent } from './components/form-news/form-news.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppfooterComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppsettingComponent,
+    FormNewsComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
